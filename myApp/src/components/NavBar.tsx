97f9a353eeb29463b8cd232d569254b0d8b1f447
mohamedElC <amranimohamad040@gmail.com>
2023-02-26 15:36:30 +0100

    upsating emails

--- myApp/src/components/NavBar.tsx
+++ myApp/src/components/NavBar.tsx
@@ -2,10 +2,8 @@
 import * as React from 'react';
 
 import { Link } from 'react-router-dom';
-type Props = {
-    
-};
-export const NavBar = (props: Props) => {
+
+export const NavBar = () => {
     return (
         <header className= ' z-30 bg-purple-900 w-full p-3 fixed top-0 font-Poppin'>
             <ul className=' font-bold text-back flex gap-3'>
--- myApp/src/emails/send.js
+++ myApp/src/emails/send.js
@@ -5,13 +5,13 @@ import { Email } from "smtpjs"
 export function sendEmail()
 {
     Email.send({
-      Host: "amranimohamad040.gmail.com",
-      Username: "amranimohamad040@gmail.com",
-      Password: "0604230466--a",
+      Host: "smtp.gmail.com",
+      Username: "elamranimohammed104@gmail.com",
+      Password: "12345678999",
       To: 'amranimohamad040@gmail.com',
-      From: "amranimohamad040@gmail.com",
+      From: "elamranimohammed104@gmail.com",
       Subject: "Sending Email using javascript",
-      Body: "Well that was easy!!",
+      Body: "Well that was easy you reseaved this one !!",
     })
       .then( (message) => {
         alert("mail sent successfully")
